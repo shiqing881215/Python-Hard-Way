7fe7e4b093f04db27fcab7e5aa06d3e9a3b27fc2
shiqing881215 <shiqing881215@gmail.com>
2017-06-13 22:37:42 -0700

    ex10 - 11

---
+++ ex10.py
@@ -0,0 +1,22 @@
+singleQuote = 'I am 6\'12" tall'
+doubleQuote = "I am 6'12\" tall"
+
+print singleQuote
+print doubleQuote
+
+tab = "\tI'm tabbed in"
+persian = "I'm split\non line"
+backslash = "I'm a \\ cat \\"
+
+# Use triple quotes keep the stype inside of it
+fat = """
+I can do whatever style here
+Like add a new line
+\t add a tab
+\\ \\\\
+"""
+
+print tab
+print persian
+print backslash
+print fat
---
+++ ex11.py
@@ -0,0 +1,11 @@
+print "How old are you?"
+age = raw_input()
+print "How tall are you?"
+height = raw_input()
+print "How much do you weigh?"
+weight = raw_input()
+
+print "You're %r old, %r tall and %r weight" %(age, height, weight)
+
+# You can also do something like this
+# age = raw_input("How old are you?")
